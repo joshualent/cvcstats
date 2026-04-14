b1955b15c5e34239ac4ac4d2c00dc0f7ece9dd5e
Joshua Lent <joshualent83@gmail.com>
2026-04-14 00:48:42 -0400

    Initial commit. create index page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -21,7 +21,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'cvcstats',
+        title: 'CVCStats',
       },
       {
         name: 'description',
